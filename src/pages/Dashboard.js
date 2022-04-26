a6fbdd9cd5541461f99acc337f6376c54724afca
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-04-26 13:53:41 -0400

    commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Dashboard = ({ user }) => {
   return (
     <Wrapper>
       <div>
         <h3 className='title'>{user}</h3>
+        <div className='title-underline'></div>
         <h5 className='title'>Wellcome to your dashboard</h5>
+      </div>
+      <p className='title'>You have't purchased any items yet...</p>
+      <div className='btn-container'>
+        <Link to={'/products'} className='btn '>
+          Back to products
+        </Link>
       </div>
     </Wrapper>
   )
@@ -14,6 +22,9 @@ const Dashboard = ({ user }) => {
 
 const Wrapper = styled.section`
   height: 100vh;
+  .btn-container {
+    text-align: center;
+  }
 `
 
 export default Dashboard
